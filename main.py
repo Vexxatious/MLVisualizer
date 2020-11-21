4e77c15f299eb39a2ead65e4536ff6a7e548c96e
Vexxatious <samulah@hotmail.com>
2020-11-21 19:46:53 +0300

    test

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("jeff")
+from PyQt5 import uic
+from PyQt5.QtWidgets import QApplication
+
+Form, Window = uic.loadUiType("mainWindow.ui")
+
+app = QApplication([])
+window = Window()
+form = Form()
+form.setupUi(window)
+window.show()
+app.exec_()
