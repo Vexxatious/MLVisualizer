55a2ba27f0fc0e455c83e85d98eee930c70edeb5
Vexxatious <samulah@hotmail.com>
2020-12-05 14:51:32 +0300

    Change from QTDesigner to regular pyqt5

---
+++ KNN.py
@@ -0,0 +1,46 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib.animation import FuncAnimation
+from collections import Counter
+
+
+class KNN:
+    def __init__(self, dataset, k=3):
+        self.k = k
+        plt.style.use('fivethirtyeight')
+        plt.title("Click to add new points")
+        self.dataset = dataset
+        [[plt.scatter(ii[0], ii[1], s=100, color=i) for ii in dataset[i]] for i in dataset]
+        ani = FuncAnimation(plt.gcf(), self.update, interval=500)
+
+        plt.tight_layout()
+        plt.show()
+
+    def k_nearest(self, dataset, predict, k):
+        if len(dataset) >= k:
+            print("K is less than the number of groups!")
+        distances = []
+        for group in dataset:
+            for features in dataset[group]:
+                distance = np.linalg.norm(np.array(features) - np.array(predict))
+                distances.append([distance, [group, features]])
+        votes = [i[1][0] for i in sorted(distances)[:k]]
+
+        for i in sorted(distances)[1:k + 1]:
+            xs = [i[1][1][0], predict[0]]
+            ys = [i[1][1][1], predict[1]]
+            print(xs, ys)
+            plt.plot(xs, ys)
+
+        vote_result = Counter(votes).most_common(1)[0][0]
+
+        return vote_result
+
+    def update(self, i):
+        [pt] = plt.ginput()
+        plt.figtext(.5, .9, "KNN finds the closest k neighbors to classify a point", fontsize=13, ha='center')
+        pt = np.asarray(pt)
+        plt.cla()
+        self.dataset[self.k_nearest(self.dataset, pt, self.k)].append(pt)
+        [[plt.scatter(ii[0], ii[1], s=100, color=i) for ii in self.dataset[i]] for i in self.dataset]
+
--- main.py
+++ main.py
@@ -1,11 +1,142 @@
-from PyQt5 import uic
-from PyQt5.QtWidgets import QApplication
+from KNN import KNN
+from PyQt5 import QtCore, QtGui, QtWidgets
 
-Form, Window = uic.loadUiType("mainWindow.ui")
+Algorithms = {"K Nearest Neighbor": KNN}
+Datasets = {"K Nearest Neighbor": [{'k': [[1,3],[2,1],[1,4],[2,2]], 'r':[[6,5],[6,7],[8,6],[7,7]]}]}
 
-app = QApplication([])
-window = Window()
-form = Form()
-form.setupUi(window)
-window.show()
-app.exec_()
+current_algorithm = None
+current_dataset = None
+
+class KNNGUI(object):
+    def setupUi(self, Dialog):
+        Dialog.setObjectName("Dialog")
+        Dialog.resize(280, 113)
+        self.horizontalLayoutWidget = QtWidgets.QWidget(Dialog)
+        self.horizontalLayoutWidget.setGeometry(QtCore.QRect(0, 0, 281, 61))
+        self.horizontalLayoutWidget.setObjectName("horizontalLayoutWidget")
+        self.horizontalLayout = QtWidgets.QHBoxLayout(self.horizontalLayoutWidget)
+        self.horizontalLayout.setContentsMargins(0, 0, 0, 0)
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.label = QtWidgets.QLabel(self.horizontalLayoutWidget)
+        self.label.setObjectName("label")
+        self.horizontalLayout.addWidget(self.label)
+        self.KCounter = QtWidgets.QSpinBox(self.horizontalLayoutWidget)
+        self.KCounter.setObjectName("KCounter")
+        self.horizontalLayout.addWidget(self.KCounter)
+        self.gridLayoutWidget = QtWidgets.QWidget(Dialog)
+        self.gridLayoutWidget.setGeometry(QtCore.QRect(0, 60, 281, 51))
+        self.gridLayoutWidget.setObjectName("gridLayoutWidget")
+        self.gridLayout = QtWidgets.QGridLayout(self.gridLayoutWidget)
+        self.gridLayout.setContentsMargins(0, 0, 0, 0)
+        self.gridLayout.setObjectName("gridLayout")
+        self.RunButton = QtWidgets.QPushButton(self.gridLayoutWidget)
+        self.RunButton.setObjectName("RunButton")
+        self.gridLayout.addWidget(self.RunButton, 0, 0, 1, 1)
+
+        self.retranslateUi(Dialog)
+        QtCore.QMetaObject.connectSlotsByName(Dialog)
+
+    def retranslateUi(self, Dialog):
+        _translate = QtCore.QCoreApplication.translate
+        Dialog.setWindowTitle(_translate("Dialog", "Dialog"))
+        self.label.setText(_translate("Dialog", "Choose K"))
+        self.RunButton.setText(_translate("Dialog", "Run"))
+
+class Ui_Dialog(object):
+    def setupUi(self, Dialog):
+        Dialog.setObjectName("Dialog")
+        Dialog.resize(621, 378)
+        self.gridLayoutWidget = QtWidgets.QWidget(Dialog)
+        self.gridLayoutWidget.setGeometry(QtCore.QRect(-1, -1, 621, 381))
+        self.gridLayoutWidget.setObjectName("gridLayoutWidget")
+        self.gridLayout = QtWidgets.QGridLayout(self.gridLayoutWidget)
+        self.gridLayout.setContentsMargins(0, 0, 0, 0)
+        self.gridLayout.setObjectName("gridLayout")
+        self.label_2 = QtWidgets.QLabel(self.gridLayoutWidget)
+        self.label_2.setObjectName("label_2")
+        self.gridLayout.addWidget(self.label_2, 1, 0, 1, 1)
+        self.horizontalLayout = QtWidgets.QHBoxLayout()
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.newModel = QtWidgets.QCheckBox(self.gridLayoutWidget)
+        self.newModel.setObjectName("newModel")
+        self.horizontalLayout.addWidget(self.newModel)
+        self.LoadModel = QtWidgets.QPushButton(self.gridLayoutWidget)
+        self.LoadModel.setObjectName("LoadModel")
+        self.horizontalLayout.addWidget(self.LoadModel)
+        self.gridLayout.addLayout(self.horizontalLayout, 1, 2, 1, 1)
+        self.AlgorithmCombo = QtWidgets.QComboBox(self.gridLayoutWidget)
+        self.AlgorithmCombo.setObjectName("AlgorithmCombo")
+        self.AlgorithmCombo.addItems(["Linear Regression","K Nearest Neighbor","Support Vector Machine"])
+        self.gridLayout.addWidget(self.AlgorithmCombo, 0, 2, 1, 1)
+        self.label_5 = QtWidgets.QLabel(self.gridLayoutWidget)
+        self.label_5.setObjectName("label_5")
+        self.gridLayout.addWidget(self.label_5, 2, 0, 1, 1)
+        self.label = QtWidgets.QLabel(self.gridLayoutWidget)
+        self.label.setObjectName("label")
+        self.gridLayout.addWidget(self.label, 0, 0, 1, 1)
+        self.horizontalLayout_2 = QtWidgets.QHBoxLayout()
+        self.horizontalLayout_2.setObjectName("horizontalLayout_2")
+        self.DatasetCombo = QtWidgets.QComboBox(self.gridLayoutWidget)
+        self.DatasetCombo.setObjectName("DatasetCombo")
+        self.horizontalLayout_2.addWidget(self.DatasetCombo)
+        self.LoadDataset = QtWidgets.QPushButton(self.gridLayoutWidget)
+        self.LoadDataset.setObjectName("LoadDataset")
+        self.horizontalLayout_2.addWidget(self.LoadDataset)
+        self.gridLayout.addLayout(self.horizontalLayout_2, 2, 2, 1, 1)
+        self.ContinueButton = QtWidgets.QPushButton(self.gridLayoutWidget)
+        self.ContinueButton.setObjectName("ContinueButton")
+        self.gridLayout.addWidget(self.ContinueButton, 3, 0, 1, 1)
+
+        self.newModel.stateChanged.connect(self.checkLoadButton)
+        self.AlgorithmCombo.currentIndexChanged.connect(self.checkAlgorithmCombo)
+        self.ContinueButton.clicked.connect(self.checkContinueButton)
+
+        self.retranslateUi(Dialog)
+        QtCore.QMetaObject.connectSlotsByName(Dialog)
+
+    def retranslateUi(self, Dialog):
+        _translate = QtCore.QCoreApplication.translate
+        Dialog.setWindowTitle(_translate("Dialog", "Dialog"))
+        self.label_2.setText(_translate("Dialog", "Choose Model"))
+        self.newModel.setText(_translate("Dialog", "New"))
+        self.LoadModel.setText(_translate("Dialog", "Load"))
+        self.label_5.setText(_translate("Dialog", "Choose Dataset"))
+        self.label.setText(_translate("Dialog", "Choose Algorithm"))
+        self.LoadDataset.setText(_translate("Dialog", "Load"))
+        self.ContinueButton.setText(_translate("Dialog", "Continue"))
+
+    def checkLoadButton(self):
+        self.LoadModel.setEnabled(not self.newModel.isChecked())
+
+    def checkAlgorithmCombo(self):
+        if self.AlgorithmCombo.currentIndex() == 1:
+            self.newModel.setEnabled(False)
+            self.LoadModel.setEnabled(False)
+            self.DatasetCombo.addItems(["Dataset 1","Dataset 2"])
+
+    def checkContinueButton(self):
+        global current_algorithm,current_dataset
+        if self.AlgorithmCombo.currentIndex() != -1 and self.DatasetCombo.currentIndex() != -1:
+            if self.AlgorithmCombo.currentText() == "K Nearest Neighbor":
+                current_algorithm = "K Nearest Neighbor"
+                current_dataset = Datasets[self.AlgorithmCombo.currentText()][self.DatasetCombo.currentIndex()]
+                self.gui = KNNGUI()
+                self.gui.setupUi(Dialog)
+                self.gui.show()
+                self.hide()
+
+
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    Dialog = QtWidgets.QDialog()
+    ui = Ui_Dialog()
+    ui.setupUi(Dialog)
+    Dialog.show()
+    app.exec_()
+
+    if current_algorithm != None and current_dataset != None:
+        algorithm = Algorithms[current_algorithm](current_dataset)
+
