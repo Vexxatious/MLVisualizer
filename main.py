cd00419afae27a1307df7fba0cef843c9e46eb55
Vexxatious <47087478+Vexxatious@users.noreply.github.com>
2020-12-26 16:39:48 +0300

    Almost Finished Neural Networks

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from KNN import KNN
+from nn import NN
+import nn
 from LinearRegression import LinearRegression
 from PyQt5 import QtCore, QtGui, QtWidgets
 import sys
@@ -7,18 +9,87 @@ import tkinter as tk
 from tkinter import filedialog
 import random
 
-Algorithms = {"K Nearest Neighbor": KNN,"Linear Regression": LinearRegression}
+Algorithms = {"K Nearest Neighbor": KNN,"Linear Regression": LinearRegression,"Neural Network": NN}
 Datasets = {"K Nearest Neighbor": [{'r': [[1,3],[2,1],[1,4],[2,2]], 'g':[[6,5],[6,7],[8,6],[7,7]]},
                                    {'r': [[4,4],[3,4],[4,5],[3,5],[3.5,6],[3.5,4]], 'g':[[2,5],[2,4],[3.5,2],[4,2],[5,4],[5,5],[4,7],[3,3],[2.5,3],[2.5,6],[3,7],[4.5,3],[2.5,4],[5.6,6]]},
                                    {'r': [[1,3],[2,1],[1,4],[2,2]], 'g':[[6,2],[6,3],[8,1],[7,3]], 'b':[[4,7],[6,8],[3,6],[7,8]]}],
-            "Linear Regression": [[[1,2],[2,3],[4,5],[6,7],[8,9]]]}
+            "Linear Regression": [[[1,2],[2,3],[4,5],[6,7],[8,9]]],
+            "Neural Network": ["mnist"]}
 
 current_algorithm = ""
 current_dataset = []
 parameters = []
+model_loaded = False
+model_path = ""
 
+class NNGUI(object):
+    def setupUi(self, Dialog):
+        Dialog.setFixedSize(397,297)
+        Dialog.setObjectName("Dialog")
+        Dialog.resize(397, 297)
+        self.gridLayoutWidget = QtWidgets.QWidget(Dialog)
+        self.gridLayoutWidget.setGeometry(QtCore.QRect(-1, -1, 401, 301))
+        self.gridLayoutWidget.setObjectName("gridLayoutWidget")
+        self.gridLayout = QtWidgets.QGridLayout(self.gridLayoutWidget)
+        self.gridLayout.setContentsMargins(0, 0, 0, 0)
+        self.gridLayout.setObjectName("gridLayout")
+        self.epoch_counter = QtWidgets.QSpinBox(self.gridLayoutWidget)
+        self.epoch_counter.setObjectName("epoch_counter")
+        self.epoch_counter.setValue(10)
+        self.gridLayout.addWidget(self.epoch_counter, 1, 1, 1, 1)
+        self.activation_combo = QtWidgets.QComboBox(self.gridLayoutWidget)
+        self.activation_combo.setObjectName("activation_combo")
+        self.activation_combo.addItem("")
+        self.activation_combo.addItem("")
+        self.activation_combo.addItem("")
+        self.activation_combo.addItem("")
+        self.gridLayout.addWidget(self.activation_combo, 0, 1, 1, 1)
+        self.label_2 = QtWidgets.QLabel(self.gridLayoutWidget)
+        self.label_2.setObjectName("label_2")
+        self.gridLayout.addWidget(self.label_2, 1, 0, 1, 1)
+        self.label_3 = QtWidgets.QLabel(self.gridLayoutWidget)
+        self.label_3.setObjectName("label_3")
+        self.gridLayout.addWidget(self.label_3, 2, 0, 1, 1)
+        self.label = QtWidgets.QLabel(self.gridLayoutWidget)
+        self.label.setObjectName("label")
+        self.gridLayout.addWidget(self.label, 0, 0, 1, 1)
+        self.layers = QtWidgets.QLineEdit(self.gridLayoutWidget)
+        self.layers.setObjectName("layers")
+        self.gridLayout.addWidget(self.layers, 2, 1, 1, 1)
+        self.pushButton = QtWidgets.QPushButton(self.gridLayoutWidget)
+        self.pushButton.setObjectName("pushButton")
+        self.gridLayout.addWidget(self.pushButton, 3, 1, 1, 1)
+
+        self.retranslateUi(Dialog)
+        QtCore.QMetaObject.connectSlotsByName(Dialog)
+        self.pushButton.clicked.connect(self.runButtonCheck)
+    def retranslateUi(self, Dialog):
+        _translate = QtCore.QCoreApplication.translate
+        Dialog.setWindowTitle(_translate("Dialog", "Dialog"))
+        self.activation_combo.setItemText(0, _translate("Dialog", "relu"))
+        self.activation_combo.setItemText(1, _translate("Dialog", "sigmoid"))
+        self.activation_combo.setItemText(2, _translate("Dialog", "tanh"))
+        self.activation_combo.setItemText(3, _translate("Dialog", "linear"))
+        self.label_2.setText(_translate("Dialog", "Epochs:"))
+        self.label_3.setText(_translate("Dialog", "Layers ( neurons, comma seperated )"))
+        self.label.setText(_translate("Dialog", "Activation function: "))
+        self.layers.setText(_translate("Dialog", "128,128"))
+        self.pushButton.setText(_translate("Dialog", "Run"))
+    
+    def runButtonCheck(self):
+        global parameters
+        if self.epoch_counter.value() < 1:
+            ctypes.windll.user32.MessageBoxW(0,"Epochs cannot be less than 1","Warning",0)
+            return
+        parameters.append([int(i) for i in self.layers.text().split(',')])
+        parameters.append(self.activation_combo.currentText())
+        parameters.append(self.epoch_counter.value())
+        Dialog.accept()
+        
+        
 class KNNGUI(object):
     def setupUi(self, Dialog):
+        Dialog.setFixedSize(280,113)
         Dialog.setObjectName("Dialog")
         Dialog.resize(280, 113)
         self.horizontalLayoutWidget = QtWidgets.QWidget(Dialog)
@@ -43,6 +114,7 @@ class KNNGUI(object):
         self.RunButton.setObjectName("RunButton")
         self.gridLayout.addWidget(self.RunButton, 0, 0, 1, 1)
 
+
         self.RunButton.clicked.connect(self.runButtonCheck)
 
         self.retranslateUi(Dialog)
@@ -64,6 +136,7 @@ class KNNGUI(object):
 
 class Ui_Dialog(object):
     def setupUi(self, Dialog):
+        Dialog.setFixedSize(621,378)
         Dialog.setObjectName("Dialog")
         Dialog.resize(621, 378)
         self.gridLayoutWidget = QtWidgets.QWidget(Dialog)
@@ -86,7 +159,7 @@ class Ui_Dialog(object):
         self.gridLayout.addLayout(self.horizontalLayout, 1, 2, 1, 1)
         self.AlgorithmCombo = QtWidgets.QComboBox(self.gridLayoutWidget)
         self.AlgorithmCombo.setObjectName("AlgorithmCombo")
-        self.AlgorithmCombo.addItems(["Linear Regression","K Nearest Neighbor","Support Vector Machine"])
+        self.AlgorithmCombo.addItems(["Linear Regression","K Nearest Neighbor","Neural Network"])
         self.gridLayout.addWidget(self.AlgorithmCombo, 0, 2, 1, 1)
         self.label_5 = QtWidgets.QLabel(self.gridLayoutWidget)
         self.label_5.setObjectName("label_5")
@@ -111,10 +184,10 @@ class Ui_Dialog(object):
         self.gridLayout.addWidget(self.ContinueButton, 3, 0, 1, 1)
 
         self.LoadDataset.clicked.connect(self.checkLoadDataset)
-        self.newModel.stateChanged.connect(self.checkLoadButton)
+        self.newModel.stateChanged.connect(self.checkNewModel)
         self.AlgorithmCombo.currentIndexChanged.connect(self.checkAlgorithmCombo)
         self.ContinueButton.clicked.connect(self.checkContinueButton)
-
+        self.LoadModel.clicked.connect(self.checkLoadModel)
         self.retranslateUi(Dialog)
         QtCore.QMetaObject.connectSlotsByName(Dialog)
 
@@ -129,7 +202,19 @@ class Ui_Dialog(object):
         self.LoadDataset.setText(_translate("Dialog", "Load"))
         self.ContinueButton.setText(_translate("Dialog", "Continue"))
 
-    def checkLoadButton(self):
+    def checkLoadModel(self):
+        global model_loaded,model_path
+        root = tk.Tk()
+        root.withdraw()
+        file_path = filedialog.askdirectory()
+        model_loaded = True
+        model_path = file_path
+        self.ContinueButton.setEnabled(True)
+        self.newModel.setEnabled(False)
+
+    def checkNewModel(self):
+        if self.newModel.isEnabled():
+            self.ContinueButton.setEnabled(self.newModel.isChecked())
         self.LoadModel.setEnabled(not self.newModel.isChecked())
 
     def checkAlgorithmCombo(self):
@@ -137,11 +222,20 @@ class Ui_Dialog(object):
         if self.AlgorithmCombo.currentText() == "K Nearest Neighbor":
             self.newModel.setEnabled(False)
             self.LoadModel.setEnabled(False)
+            self.ContinueButton.setEnabled(True)
             self.DatasetCombo.addItems(["2 groups separate","2 groups surrounded","3 groups"])
-        if self.AlgorithmCombo.currentText() == "Linear Regression":
+        elif self.AlgorithmCombo.currentText() == "Linear Regression":
             self.newModel.setEnabled(False)
             self.LoadModel.setEnabled(False)
+            self.ContinueButton.setEnabled(True)
             self.DatasetCombo.addItems(["Test"])
+        elif self.AlgorithmCombo.currentText() == "Neural Network":
+            self.newModel.setEnabled(True)
+            self.LoadModel.setEnabled(True)
+            self.ContinueButton.setEnabled(False)
+            self.LoadDataset.setEnabled(False)
+            self.DatasetCombo.addItems(["Handwritten digits"])
+
 
     def checkContinueButton(self):
         global current_algorithm,current_dataset
@@ -198,9 +292,20 @@ if __name__ == "__main__":
 
     if current_algorithm != None and current_dataset != None:
         if current_algorithm == "K Nearest Neighbor":
+            Dialog = QtWidgets.QDialog()
             gui = KNNGUI()
             gui.setupUi(Dialog)
             Dialog.show()
             app.exec_()
-        algorithm = Algorithms[current_algorithm](current_dataset,parameters)
+        if current_algorithm == "Neural Network" and not model_loaded:
+            Dialog = QtWidgets.QDialog()
+            gui = NNGUI()
+            gui.setupUi(Dialog)
+            Dialog.show()
+            app.exec_()
+        elif current_algorithm == "Neural Network" and model_loaded:
+            algorithm = NN(current_dataset,parameters,model=model_path)
+    algorithm = Algorithms[current_algorithm](current_dataset,parameters)
+    if current_algorithm == "Neural Network":
+        nn.start()
 
---
+++ nn.py
@@ -0,0 +1,126 @@
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import numpy as np
+import pygame
+import sys
+import ctypes
+import tkinter as tk
+from tkinter import filedialog
+import cv2
+
+screen = None
+size = 0
+width = 0
+height = 0
+font = None
+nn = None
+
+def init_screen():
+    global screen , size ,width, height, font
+    pygame.font.init()
+    size = width, height = 1600, 900
+    screen = pygame.display.set_mode(size)
+
+    font = pygame.font.SysFont('arial', 32)
+class NN:
+    def __init__(self, dataset, parameters,model = None):
+        global nn
+        nn = self
+        init_screen()
+        self.user_image = None
+        self.prediction = -1
+        self.training = True
+        self.mnist = tf.keras.datasets.mnist
+        self.parameters = parameters
+        self.parameters[0].append(10)
+        (self.x_train, self.y_train), (self.x_test, self.y_test) = self.mnist.load_data()
+        self.x_train = tf.keras.utils.normalize(self.x_train, axis=1)
+        self.x_test = tf.keras.utils.normalize(self.x_test, axis=1)
+        if not model:
+            self.model = tf.keras.models.Sequential()
+            self.model.add(tf.keras.layers.Flatten())
+            for i in range(len(self.parameters[0])):
+                self.model.add(tf.keras.layers.Dense(self.parameters[0][i], activation=eval("tf.nn." + self.parameters[1])))
+            self.model.add(tf.keras.layers.Dense(10, activation=tf.nn.softmax))
+            self.model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+            self.train()
+        else:
+            self.model = tf.keras.models.load_model(model)
+            self.training = False
+    def draw(self, epoch):
+        screen.fill((0, 0, 0))
+
+
+        for i in range(len(self.parameters[0])):
+            r = height / (4 * min(self.parameters[0][i], 10))
+            x = width / len(self.parameters[0])
+            for j in range(min(self.parameters[0][i], 10)):
+                pygame.draw.circle(screen, (255, 255, 255), (x + i * x - 2 * r, r + j * r * 4), r)
+                if i <= 1:
+                    for w in range(min(10, len(np.asarray(self.model.layers[i + 1].weights[0][j])))):
+                        pygame.draw.line(screen,
+                                         (255, 0, 0) if np.sign(self.model.layers[i + 1].weights[0][j][w]) == -1 else (
+                                         0, 0, 255), (x + i * x - 2 * r, r + j * r * 4),
+                                         (x + (i + 1) * x - 2 * r, r + w * r * 4),
+                                         width=min(
+                                             max(abs(int(np.asarray(self.model.layers[i + 1].weights[0][j])[w] * 50)),
+                                                 1), 4))
+
+        note_text_1 = font.render("(Only showing 10 neurons from", False, (255, 255, 255))
+        note_text_2 = font.render("each layer for simplicity)", False, (255, 255, 255))
+        screen.blit(note_text_1, (0, 780))
+        screen.blit(note_text_2, (0, 820))
+        if self.user_image:
+            screen.blit(self.user_image, (10, 500))
+            custom_image_text = font.render("Your image", False, (255, 255, 255))
+            prediction_text = font.render("Models prediction", False, (255, 255, 255))
+            predicted_text = font.render(f"{self.prediction}", False, (255, 0, 0))
+            screen.blit(predicted_text, (250, 550))
+            screen.blit(custom_image_text, (10, 450))
+            screen.blit(prediction_text, (200, 450))
+        if self.training:
+            training_text = font.render(f"Training, current epoch: {epoch + 1}/{self.parameters[2]}", False, (255, 0, 0))
+            screen.blit(training_text, (0, 0))
+        else:
+            training_text = font.render("Training completed !", False, (255, 0, 0))
+            screen.blit(training_text, (0, 0))
+            pygame.draw.rect(screen, (255, 255, 0), pygame.Rect(20, 150, 200, 70))
+            save_model_text = font.render("Save model", False, (200, 200, 200))
+            screen.blit(save_model_text, (30, 160))
+            pygame.draw.rect(screen, (0, 0, 255), pygame.Rect(20, 250, 300, 70))
+            save_model_text = font.render("Test Custom Image", False, (255, 255, 255))
+            screen.blit(save_model_text, (30, 260))
+
+        pygame.display.update()
+
+    def train(self):
+        for i in range(self.parameters[2]):
+            self.model.fit(self.x_train, self.y_train, epochs=1)
+            self.draw(i)
+        self.training = False
+
+
+def start():
+
+    while 1:
+        mouse = pygame.mouse.get_pos()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN and not nn.training:
+                if 20 < mouse[0] < 220 and 150 < mouse[1] < 220:
+                    nn.model.save("Saved Model")
+                    ctypes.windll.user32.MessageBoxW(0, "Model saved successfully!", "Model Saved", 0)
+                elif 20 < mouse[0] < 270 and 250 < mouse[1] < 320:
+                    root = tk.Tk()
+                    root.withdraw()
+                    file = filedialog.askopenfilename()
+                    img = cv2.imread(file, cv2.IMREAD_GRAYSCALE)
+                    img = np.array([img])
+                    img = img / 255.0
+                    nn.prediction = np.argmax(nn.model.predict(img))
+                    print(nn.prediction)
+                    nn.user_image = pygame.image.load(file)
+                    nn.user_image = pygame.transform.scale(nn.user_image, (100, 100))
+
+        nn.draw(-1)
